068bf22fe6e53a2fdb57cad95ed7ec76670b40db
omardevm <omardguez.contacto@gmail.com>
2021-05-24 12:00:18 -0500

    home and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarModule } from './navbar/navbar.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NavbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
